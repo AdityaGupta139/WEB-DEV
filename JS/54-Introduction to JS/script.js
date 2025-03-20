574b2f17ac06efaf3a9fe05d7a9303313b95583d
ADITYA GUPTA <gaditya2005@gmail.com>
2025-03-20 23:59:56 +0530

    Create script.js

---
+++ JS/54-Introduction to JS/script.js
@@ -0,0 +1,14 @@
+alert("hello world")
+console.log("code is running")
+console.log("code is also running")
+console.log("code is running fine")
+var a=prompt("enter your number")
+var isTrue = confirm("Are you sure to leave this page and blast your computer")
+
+if(isTrue){
+    console.log("computer is blasting")
+}
+else{
+    console.log("your computer is not blasting")
+}
+console.log("your number is" + a)
