f0cc48ae150fed05d24c4aabc6b42ec43ed93dcf
ADITYA GUPTA <gaditya2005@gmail.com>
2025-03-21 12:33:17 +0530

    .

---
+++ JS/55-JS Variables,Data types and Objects/script.js
@@ -0,0 +1,36 @@
+console.log("it's 2nd file of js")
+let a=5;
+a=a+1;
+let b=6;
+let c="aditya";
+let _a="parth";
+// var 55a="rohan"; not allowed
+console.log(a+b+8);
+console.log(typeof a,typeof b,typeof c);
+{
+    let a=89;
+    console.log(a)
+}
+const a1=6;
+// a1=a1+1; not allowed because a1 is constant
+
+let x="Aditya"
+let y=22
+let z=3.55
+const p=true;
+let q= undefined;
+let r=null;
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+let o={
+    "name":"aditya",
+    "job code":5600,
+    "is_handsome":true
+
+}
+console.log(o)
+o.salary="100 crores"
+console.log(o)
+o.salary="500 crores";
+console.log(o)
