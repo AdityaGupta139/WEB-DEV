e34fd3d9326895cf7bcdae6b3b9d7cdf69d88ac3
ADITYA GUPTA <gaditya2005@gmail.com>
2025-03-31 13:35:39 +0530

    Create script.js

---
+++ JS/60-Strings in JS/script.js
@@ -0,0 +1,17 @@
+console.log("this file is about strings in JS")
+let a="Aditya"
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+let his_name="aditya"
+let friend="piyush"
+console.log(`his name is ${his_name} and his friend name is ${friend}`)
+let b="Praful"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.slice(1,5))
+console.log(b.replace("Pr","Ch"))
+console.log(b.concat("pagal"))
