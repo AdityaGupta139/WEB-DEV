5314e39cfed94f706262c29220c32d6aff24c33a
ADITYA GUPTA <gaditya2005@gmail.com>
2025-04-02 22:18:50 +0530

    .

---
+++ JS/63-JS Arrays/script.js
@@ -0,0 +1,10 @@
+let arr=[1,2,3,4,5]
+console.log(arr)
+console.log(arr.length)
+console.log(arr.toString)
+console.log(arr.join("and"))
+console.log(arr.push(9))
+let a1=[2,3,4]
+let a2=[44,3,4]
+let a3=[67,3,4]
+console.log(a1.concat(a2.a3))
