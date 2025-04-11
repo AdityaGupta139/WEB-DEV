2b8d5e67fc3c54cf15a1b1118191013efd792521
ADITYA GUPTA <gaditya2005@gmail.com>
2025-04-11 14:26:33 +0530

    .

---
+++ JS/68-Selecting by Ids,Classes and More/script.js
@@ -0,0 +1,14 @@
+console.log("ADITYA")
+// let boxes=document.getElementsByClassName("box")
+// console.log(boxes)
+// boxes[2].style.backgroundColor="yellow"
+
+
+// document.getElementById("color").style.backgroundColor="red"
+
+// document.querySelector(".box").style.backgroundColor="green"
+
+
+document.querySelectorAll(".box").forEach(e=>{
+    e.style.backgroundColor="aqua"
+})
