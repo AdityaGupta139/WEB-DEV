49ed1d29463c78ce62dd14ff164558b5257c3ca9
ADITYA GUPTA <gaditya2005@gmail.com>
2025-04-19 22:37:00 +0530

    .

---
+++ BACKEND/91-Exercise-Clear the clutter/index.js
@@ -0,0 +1,52 @@
+import fs from "fs/promises"
+import fsn from "fs"
+import path from "path"
+
+
+const basepath="//Users//adityagupta//Desktop//WEB-DEV//BACKEND//91-Exercise-Clear the clutter"
+
+
+
+let files=await fs.readdir(basepath)
+
+for (const item of files) {
+    console.log("running for",item)
+    let ext=item.split(".")[item.split(".").length-1]
+    console.log(ext)
+    if(ext!="js" && ext!="json" && item.split(".").length>1){
+    if(fsn.existsSync(path.join(basepath,ext))){
+        fs.rename(path.join(basepath,item)),path.join(basepath,ext,item)
+
+    }
+    else{
+        fs.mkdir(ext)
+        fs.rename(path.join(basepath,item),path.join(basepath,ext,item))
+    }
+}
+    
+}
+// import fs from "fs/promises"
+// import fsn from "fs"
+// import path from "path"
+
+// const basepath = "//Users//adityagupta//Desktop//WEB-DEV//BACKEND//91-Exercise-Clear the clutter"
+
+// let files = await fs.readdir(basepath)
+
+// for (const item of files) {
+//     console.log("running for ", item)
+//     let ext = item.split(".")[item.split(".").length - 1]
+//     if (ext != "js" && ext != "json" && item.split(".").length > 1) {
+
+
+//         if (fsn.existsSync(path.join(basepath, ext))) {
+//             // Move the file to this directory if its not a js or json file
+//             fs.rename(path.join(basepath, item), path.join(basepath, ext, item))
+//         }
+//         else {
+//             fs.mkdir(ext)
+//             fs.rename(path.join(basepath, item), path.join(basepath, ext, item))
+//         }
+//     }
+
+// }
