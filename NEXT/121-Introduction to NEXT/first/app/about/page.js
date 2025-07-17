ec4843d9395e6d3d2addb7e42a06cb41fb39f47c
ADITYA GUPTA <167164745+AdityaGupta139@users.noreply.github.com>
2025-07-17 13:03:16 +0530

    .

---
+++ NEXT/121-Introduction to NEXT/first/app/about/page.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1 className="text-xl">Problems Solved by Next.js</h1>
+      <ul>
+        <li>Full Stack Solution</li>
+        <li>File Based Routing</li>
+        <li>Optimized Rendering</li>  
+        <li>Additional features like routers from next/navigation</li>  
+        
+      </ul>
+    </div>
+  )
+}
+
+export default page
