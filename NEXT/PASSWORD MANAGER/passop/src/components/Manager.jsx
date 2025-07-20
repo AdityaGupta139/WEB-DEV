7cf66d62773299531717d086f6d507630f5269c7
ADITYA GUPTA <167164745+AdityaGupta139@users.noreply.github.com>
2025-07-20 17:22:40 +0530

    Update Manager.jsx

--- NEXT/PASSWORD MANAGER/passop/src/components/Manager.jsx
+++ NEXT/PASSWORD MANAGER/passop/src/components/Manager.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
-
+import { useRef,useState } from 'react'
 const Manager = () => {
+    const ref = useRef()
+    const [form, setform] = useState({site:"",username:"",password:""})
+    const showPassword = () => {
+        alert("Show Password")
+        if (ref.current.src.includes("/eyenot.png")) {
+            ref.current.src = "/eye.png"
+        }
+        else {
+            ref.current.src = "/eyenot.png"
+        }
+        
+    }
+
+    const savePassword=() => {
+      console.log(form)
+    }
+    
+
+    const handleChange =(e) => {
+      setform({...form,[e.target.name]:e.target.value})
+    }
+    
+
+
     return (
         <>
-            <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#62d2ff_100%)]"></div>
+            <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#62d2ff_100%)]"></div>
 
 
             <div className="mycontainer bg-white/10 backdrop-blur-sm rounded-lg my-4 text-white border solid border-[#3a9ac756] shadow-4xl ">
@@ -17,14 +41,20 @@ const Manager = () => {
 
                 <div className='text-black flex flex-col gap-8 p-4 items-center'>
 
-                    <input className=' border solid border-[#3a9bc7] bg-[#d7ebf5]  w-full p-4 py-1 rounded-xl' type="text" placeholder='Enter Website URL' />
+                    <input value={form.site} onChange={handleChange} className=' border solid border-[#3a9bc7] bg-[#d7ebf5]  w-full p-4 py-1 rounded-xl' type="text" name='site' placeholder='Enter Website URL' />
 
                     <div className='flex gap-8 w-full justify-between'>
 
-                        <input className=' border solid border-[#deeff7] bg-[#d7ebf5]  w-full p-4 py-1 rounded-xl' type="text" placeholder='Enter Username'/>
-                        <input className=' border solid border-[#3a9bc7] bg-[#d7ebf5]  w-full p-4 py-1 rounded-xl' type="text" placeholder=' Enter Password'/>
+                        <input value={form.username} onChange={handleChange} className=' border solid border-[#deeff7] bg-[#d7ebf5]  w-full p-4 py-1 rounded-xl' type="text" name='username' placeholder='Enter Username' />
+                        <div className="relative">
+
+                            <input value={form.password} onChange={handleChange} className=' border solid border-[#3a9bc7] bg-[#d7ebf5]  w-full p-4 py-1 rounded-xl' type="text" name='password' placeholder=' Enter Password' />
+                            <span className="absolute right-[1px] top-[3px] hover:cursor-pointer" onClick={showPassword}>
+                                <img ref={ref} className='p-1 ' width={30} src="/eye.png" alt="" />
+                            </span>
+                        </div>
                     </div>
-                    <button className='flex items-center gap-2 justify-center [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#62d2ff_100%)] text-white p-2 rounded-xl hover:font-bold transition-all  w-fit px-8  border solid border-[#3a9ac756]'>
+                    <button onClick={savePassword} className='flex items-center gap-2 justify-center [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#62d2ff_100%)] text-white p-2 rounded-xl hover:font-bold transition-all  w-fit px-8  border solid border-[#3a9ac756]'>
                         <lord-icon
                             src="https://cdn.lordicon.com/tsrgicte.json"
                             trigger="hover"
